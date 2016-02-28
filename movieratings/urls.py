28af798e6652cb4cf9d562f60ae08199fc9a4ab5
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-02-28 16:49:03 -0500

    added all_movies view, added add own rating functionality and logic to update average rating

--- movieratings/urls.py
+++ movieratings/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from mr_app.views import index_view, movie_view, rater_view
+from mr_app.views import index_view, movie_view, rater_view, all_movies
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index_view),
+    url(r'^all$', all_movies),
     url(r'^movies/(\d+)$', movie_view),
     url(r'^rater/(\d+)$', rater_view)
 ]
--- mr_app/views.py
+++ mr_app/views.py
@@ -1,5 +1,7 @@
+from django.db.models import Avg
 from django.shortcuts import render
 from mr_app.models import Movie, Rater, Rating
+from datetime import datetime
 
 
 def index_view(request):
@@ -9,7 +11,19 @@ def index_view(request):
 
 def movie_view(request, re_capture):
     movie = Movie.objects.get(pk=re_capture)
-    movie_ratings = Rating.objects.filter(movie=movie.pk)
+    rater = Rater.objects.get(pk=944)
+    rating = request.POST.get('rating')
+
+    if rating and not Rating.objects.filter(movie=movie, rater=rater):
+        Rating.objects.create(rater=rater, movie=movie,
+                              rating=rating, timestamp=int(datetime.timestamp(datetime.now())))
+
+    # recalculate the movies average rating
+    avg_rating = Rating.objects.filter(movie=movie).aggregate(Avg('rating'))
+    movie.avg_rating = avg_rating['rating__avg']
+    movie.save()
+
+    movie_ratings = Rating.objects.filter(movie=movie.pk).order_by('-timestamp')
 
     return render(request, 'movie.html', {
                                         'movie': movie,
@@ -18,9 +32,14 @@ def movie_view(request, re_capture):
 
 def rater_view(request, re_capture):
     rater = Rater.objects.get(pk=re_capture)
-    rater_movies = Rating.objects.filter(rater=rater.pk)
+    rater_movies = Rating.objects.filter(rater=rater.pk).order_by('-timestamp')
 
     return render(request, 'rater.html', {
                                         'rater': rater,
                                         'rater_movies' : rater_movies
     })
+
+def all_movies(request):
+    all_movies = Movie.objects.all().order_by('title')
+
+    return render(request, 'all_movies.html', {'all_movies': all_movies})
