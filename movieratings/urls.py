79a57aaa41e2bc13e1fc41862e7c2f1dfa301e9c
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-03-15 15:57:52 -0400

    added urls and views for api access via GET and POST methods

--- movieratings/urls.py
+++ movieratings/urls.py
@@ -15,12 +15,19 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from mr_app.views import index_view, movie_view, rater_view, all_movies
+from mr_app.views import index_view, movie_view, rater_view, all_movies, movie_api_view,\
+    rater_api_view, rating_api_view, specific_movie_api_view, specific_rater_api_view, specific_rating_api_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index_view, name='index_view'),
     url(r'^all$', all_movies, name='all_movies'),
     url(r'^movies/(\d+)$', movie_view, name='movie_view'),
-    url(r'^rater/(\d+)$', rater_view, name='rater_view')
+    url(r'^rater/(\d+)$', rater_view, name='rater_view'),
+    url(r'^api/movie/$', movie_api_view, name='movie_api_view'),
+    url(r'^api/rater/$', rater_api_view, name='rater_api_view'),
+    url(r'^api/rating/$', rating_api_view, name='rating_api_view'),
+    url(r'^api/movie/(\d+)$', specific_movie_api_view, name='specific_movie_api_view'),
+    url(r'^api/rater/(?P<pk>\d+)$', specific_rater_api_view, name='specific_rater_api_view'),
+    url(r'^api/rating/(?P<pk>\d+)$', specific_rating_api_view, name='specific_rating_view'),
 ]
--- mr_app/views.py
+++ mr_app/views.py
@@ -1,7 +1,12 @@
 from django.db.models import Avg
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
+
 from mr_app.models import Movie, Rater, Rating
 from datetime import datetime
+import simplejson as json
+from django.forms.models import model_to_dict
 
 
 def index_view(request):
@@ -43,3 +48,61 @@ def all_movies(request):
     all_movies = Movie.objects.all().order_by('title')
 
     return render(request, 'all_movies.html', {'all_movies': all_movies})
+
+def movie_api_view(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        release_date = request.POST.get('release_date')
+        video_date = request.POST.get('video_date')
+        url = request.POST.get('url')
+
+        new_movie = Movie.objects.create(title=title, release_date=release_date,
+                                         video_date=video_date, url=url)
+        new_movie_dict = model_to_dict(new_movie)
+
+        return HttpResponse(json.dumps(new_movie_dict), content_type='application/json', status=201)
+
+    data = list(Movie.objects.all().values())
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def rater_api_view(request):
+    if request.method == 'POST':
+        age = request.POST['age']
+        occupation = request.POST['occupation']
+        zipcode = request.POST['zipcode']
+        sex = request.POST['sex']
+        new_rater = Rater.objects.create(age=age, occupation=occupation,
+                                         zipcode=zipcode, sex=sex)
+        new_rater_dict = model_to_dict(new_rater)
+
+        return HttpResponse(json.dumps(new_rater_dict), content_type='application/json', status=201)
+
+    data = list(Rater.objects.all().values())
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def rating_api_view(request):
+    if request.method == 'POST':
+        movie_id = request.POST.get('movie_id')
+        rating = request.POST.get('rating')
+        rater_id = request.POST.get('rater_id')
+        timestamp = request.POST.get('timestamp')
+        new_rating = Rating.objects.create(movie_id=movie_id, rating=rating, rater_id=rater_id, timestamp=timestamp)
+        new_rating_dict = model_to_dict(new_rating)
+
+        return HttpResponse(json.dumps(new_rating_dict), content_type='application/json', status=201)
+
+    data = list(Rating.objects.all().values())
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def specific_movie_api_view(request, pk):
+    data = list(Movie.objects.filter(pk=pk).values())
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def specific_rater_api_view(request, pk):
+    data = list(Rater.objects.filter(pk=pk).values())
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+def specific_rating_api_view(request, pk):
+    data = list(Rating.objects.filter(pk=pk).values())
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
